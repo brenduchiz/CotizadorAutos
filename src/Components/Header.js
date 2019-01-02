6925ae3c9aa47accb20a43d06bee416020ca54e8
brenda <brendamorpe@gmail.com>
2019-01-01 18:03:44 -0600

    Formulario html

---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Header extends Component {
+  render() {
+    return (
+      <div >
+        <header >
+         <h1> Cotizador de Seguro de Auto</h1> 
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Header;
